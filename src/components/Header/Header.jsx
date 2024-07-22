ce71dafe8a4f1d5be7240df52958763e2700ed4e
btissam19 <btissam1234btissam12@gmail.com>
2024-07-22 11:56:15 +0100

    final style for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,35 +4,36 @@ import head_rightImg from '../../assets/headerRght.png';
 
 const Header = () => {
   return (
-    <div className='mt-5 header'>
-      <Container>
+    <div className='header'>
+    <div className="container-fluid">
       <Row className="custom-background">
-      <Col md={6}>
-        <div className="head_left">
-          <div className="w-75 mt-2 h_title">
-            <h1>Seamless Real-time Identity Verification</h1>
+        <Col md={5}>
+          <div className="head_left">
+            <div className="w-75 mt-2 h_title text-element">
+              <h1>Seamless Real-time Identity Verification</h1>
+            </div>
+            <p className="text-secondary">
+              Discover Our Cutting-Edge Verification Solutions.
+            </p>
+            <div className="d-flex justify-content-between align-items-start w-50 mt-4">
+              <a href="#features" className="custom-button">
+                Explore our Features
+                <span>&rarr;</span>
+              </a>
+            </div>
           </div>
-          <p className="text-secondary">
-            Discover Our Cutting-Edge Verification Solutions.
-          </p>
-          <div className="d-flex justify-content-between align-items-start w-50 mt-4">
-          <a href="#features" className="custom-button">
-                 Explore our Features
-               <span>&rarr;</span>
-           </a>
+        </Col>
+        <Col md={7}>
+          <div className="head_right">
+            <div className="imageContainer">
+              <img src={head_rightImg} alt="header-image" className="head_rightImg" />
+            </div>
           </div>
-        </div>
-      </Col>
-      <Col md={6}>
-        <div className="head_right">
-          <div className="imageContainer d-flex justify-content-end align-items-center shadow-sm rounded">
-            <img src={head_rightImg} alt="header-image" className="head_rightImg" />
-          </div>
-        </div>
-      </Col>
-    </Row>
-      </Container>
+        </Col>
+      </Row>
     </div>
+  </div>
+  
 
   )
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -9,29 +9,30 @@ import { Button } from 'react-bootstrap';
 
 const Topbar = () => {
   return (
-    <div>
-      <Navbar expand="lg" className="bg_color shadow-sm">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo} alt="logo" style={{width:"100%", height:"100%", objectFit:'cover'}}/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto text-center d-flex justify-content-center align-items-center">
-              <Nav.Link href="#home" className='links'>Home</Nav.Link>
-              <Nav.Link href="#about" className='links'>About Us</Nav.Link>
-              <Nav.Link href="#contact" className='links'>Contact</Nav.Link>
-              <Nav.Link href="#testimonials" className='links'>Testimonials</Nav.Link>
-              <Nav.Item>
-                <Link to='/login' className='text-decoration-none'>
-                  <Button size="sm" className="bg_login fw-bold text-decoration-none border-0">Login</Button>
-                </Link>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+  <div>
+  <Navbar expand="lg" className="bg_color shadow-sm">
+  <Container>
+    <Navbar.Brand href="#home">
+      <img src={logo} alt="logo" style={{ width: "100%", height: "100%", objectFit: 'cover' }} />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto text-center d-flex justify-content-center align-items-center">
+        <Nav.Link href="#home" className='links active'>Home</Nav.Link>
+        <Nav.Link href="#about" className='links'>About Us</Nav.Link>
+        <Nav.Link href="#contact" className='links'>Contact</Nav.Link>
+        <Nav.Link href="#testimonials" className='links'>Testimonials</Nav.Link>
+        <Nav.Item>
+          <Link to='/login' className='text-decoration-none'>
+            <Button size="sm" className="bg_login">Login</Button>
+          </Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+</div>
   );
 }
 
