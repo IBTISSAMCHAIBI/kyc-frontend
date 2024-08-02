693190ffcfaeed4bdd1fdbb254f032cd2bcc59e6
btissam19 <btissam1234btissam12@gmail.com>
2024-08-02 16:00:36 +0100

    done with sinup and login without googell functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,37 +5,34 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../components/Login/Login.css';
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { ToastContainer, toast } from 'react-toastify';
 import { app } from '../firebase';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
 
-  const handleGoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth(app);
+  // const handleGoogleSignIn = async () => {
+  //   const provider = new GoogleAuthProvider();
+  //   const auth = getAuth(app);
 
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      const token = await user.getIdToken();
+  //   try {
+  //     const result = await signInWithPopup(auth, provider);
+  //     const user = result.user;
+  //     const token = await user.getIdToken();
 
-      const response = await axios.post('http://localhost:5000/google-login', { token });
+  //     const response = await axios.post('http://localhost:5000/google-login', { token });
 
-      localStorage.setItem('token', response.data.token);  
-      localStorage.setItem('role', response.data.role);  // Store user role
-      setMessage('Successfully authenticated with Google!');
-      setSuccess(true);
-      navigate('/process');  // Redirect to the dashboard
-    } catch (error) {
-      console.error('Error signing in with Google:', error.message);
-      setMessage('Error signing up with Google.');
-      setSuccess(false);
-    }
-  };
+  //     localStorage.setItem('token', response.data.token);  
+  //     localStorage.setItem('role', response.data.role);  // Store user role
+  //     toast.success('Successfully authenticated with Google!');
+  //     navigate('/process');  // Redirect to the dashboard
+  //   } catch (error) {
+  //     toast.error('Error signing in with Google.');
+  //   }
+  // };
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -44,8 +41,7 @@ const Login = () => {
 
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('role', response.data.role);  // Store user role
-      setMessage('Successfully logged in!');
-      setSuccess(true);
+      toast.success('Successfully logged in!');
 
       // Redirect based on user role
       const role = response.data.role;
@@ -55,8 +51,7 @@ const Login = () => {
         navigate('/process');  // Redirect to user dashboard
       }
     } catch (error) {
-      setMessage(error.response?.data?.error || 'An error occurred');
-      setSuccess(false);
+      toast.error(error.response?.data?.error || 'An error occurred');
     }
   };
 
@@ -72,24 +67,18 @@ const Login = () => {
         </Col>
         <Col md={7} className="login-right">
           <div className="login-form">
-            <Button variant="outline-primary" className="google-btn" onClick={handleGoogleSignIn}>
+            {/* <Button variant="outline-primary" className="google-btn" onClick={handleGoogleSignIn}>
               <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google Logo" /> Continue with Google
-            </Button>
+            </Button> */}
             <hr className="divider" />
             <Form onSubmit={handleLogin}>
               <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="text" placeholder="firstnam.lastnam@domain.com" value={username} onChange={(e) => setUsername(e.target.value)} />
               </Form.Group>
               <Form.Group controlId="formPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <Form.Text className="text-right">
-                  <a href="#" className="forgot-password">Forgot password?</a>
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId="formCheckbox">
-                <Form.Check type="checkbox" label="Stay logged in for one week" />
               </Form.Group>
               <Button variant="primary" type="submit" className="login-btn">
                 Login
@@ -97,11 +86,11 @@ const Login = () => {
               <p className="signup-link">
                 Don't have an account? <Link to="/SignUp">Sign Up</Link>
               </p>
-              {message && <p className={success ? 'text-success' : 'text-danger'}>{message}</p>}
             </Form>
           </div>
         </Col>
       </Row>
+      <ToastContainer />
     </Container>
   );
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,34 +4,48 @@ import subscribe from '../assets/subscribe.png';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../components/Login/Login.css';
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
-import { app } from '../firebase';
+// import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+// import { app } from '../firebase';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignUp = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const [success, setSuccess] = useState(false);
   const navigate = useNavigate(); // Initialize useNavigate hook
 
-  const handleGoogleSignUp = async () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth(app);
 
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log('Google User Info:', user);
-      setMessage('Successfully signed up with Google!');
-      setSuccess(true);
-      // Redirect to login page
-      navigate('/Login');
-    } catch (error) {
-      console.error('Error signing in with Google:', error.message);
-      setMessage('Error signing up with Google.');
-      setSuccess(false);
-    }
-  };
+//   const handleGoogleSignUp = async () => {
+//     const provider = new GoogleAuthProvider();
+//     const auth = getAuth(app);
+
+//     try {
+//         const result = await signInWithPopup(auth, provider);
+//         const user = result.user;
+//         const token = await user.getIdToken();
+
+//         console.log('Google User Info:', user);
+//         toast.success('Successfully signed up with Google!');
+
+//         // Send user info to the backend
+//         const response = await axios.post('http://localhost:5000/create_google_user', {
+//             username: user.email,
+//             password: user.displayName, 
+//         });
+        
+//         // Store token and username in local storage
+//         localStorage.setItem('token', response.data.token);  // Store the token received from backend
+//         localStorage.setItem('username', user.email);
+//         localStorage.setItem('role', 'user');
+
+//         // Redirect to process page
+//         navigate('/process');
+//     } catch (error) {
+//         console.error('Error signing in with Google:', error.message);
+//         toast.error('Error signing up with Google.');
+//     }
+// };
+
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -39,19 +53,18 @@ const SignUp = () => {
     try {
       const response = await axios.post('http://localhost:5000/create_user', { username: username, password: password });
       console.log('Response:', response.data); // Debug log
-      setMessage('Successfully signed up!');
-      setSuccess(true);
+      toast.success('Successfully signed up!');
       // Redirect to login page
       navigate('/Login');
     } catch (error) {
       console.error('Error:', error); // Debug log
-      setMessage(error.response?.data?.error || 'An error occurred');
-      setSuccess(false);
+      toast.error(error.response?.data?.error || 'An error occurred');
     }
   };
 
   return (
     <Container fluid className="login-container">
+      <ToastContainer />
       <Row className="w-100">
         <Col md={5} className="login-left">
           <div className="login-welcome">
@@ -62,24 +75,18 @@ const SignUp = () => {
         </Col>
         <Col md={7} className="login-right">
           <div className="login-form">
-            <Button variant="outline-primary" className="google-btn" onClick={handleGoogleSignUp}>
+            {/* <Button variant="outline-primary" className="google-btn" onClick={handleGoogleSignUp}>
               <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google Logo" /> Continue with Google
-            </Button>
+            </Button> */}
             <hr className="divider" />
             <Form onSubmit={handleSignUp}>
               <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="text" placeholder="firstnam.lastnam@domain.com" value={username} onChange={(e) => setUsername(e.target.value)} />
               </Form.Group>
               <Form.Group controlId="formPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <Form.Text className="text-right">
-                  <a href="#" className="forgot-password">Forgot password?</a>
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId="formCheckbox">
-                <Form.Check type="checkbox" label="Stay logged in for one week" />
               </Form.Group>
               <Button variant="primary" type="submit" className="login-btn">
                 Sign Up
@@ -87,7 +94,6 @@ const SignUp = () => {
               <p className="signup-link">
                 Do you have an account? <Link to="/Login">Login</Link>
               </p>
-              {message && <p className={success ? 'text-success' : 'text-danger'}>{message}</p>}
             </Form>
           </div>
         </Col>
