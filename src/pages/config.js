4dca83f423e829284baa8970c57b0474c66058a6
btissam19 <btissam1234btissam12@gmail.com>
2024-08-11 17:55:49 +0100

    enncing live detection page

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 export const CONFIG = {
-    // BASE_URL: 'https://kyc-backend-api-bfb5ecbf820e.herokuapp.com',
-    BASE_URL:'http://127.0.0.1:5000'
+    BASE_URL: 'https://kyc-backend-api-bfb5ecbf820e.herokuapp.com',
+    // BASE_URL:'http://127.0.0.1:5000'
   };
   
